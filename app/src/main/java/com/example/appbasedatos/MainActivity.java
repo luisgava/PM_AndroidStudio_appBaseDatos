bbb30bf89e9406444fc5640a6918422a91fc617d
luisgava <luisgargallovaamonde@gmail.com>
2021-01-13 10:15:22 +0100

    actualizacion

--- app/src/main/java/com/example/appbasedatos/MainActivity.java
+++ app/src/main/java/com/example/appbasedatos/MainActivity.java
@@ -1,14 +1,48 @@
 package com.example.appbasedatos;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.EditText;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    TextView txtTexto;
+    SQLiteDatabase db;
+    SQLiteHelper helper;
+    EditText edtTitulo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtTexto=findViewById(R.id.txtValor);
+
+        //Para que el textview tenga scroll
+        txtTexto.setMovementMethod(new ScrollingMovementMethod());
+
+        edtTitulo=findViewById((R.id.edtBusqueda));
+
+        helper = new SQLiteHelper(this);
+
+        // Habilitamos la bd para poder escribir
+        db=helper.getWritableDatabase();
+
+        //Creamos los datos de la tabla
+        ContentValues values = new ContentValues();
+        values.put(EstructuraBD.EstructuraOperas.COLUMN_NAME_TITULO, "Don Giovani");
+        values.put(EstructuraBD.EstructuraOperas.COLUMN_NAME_COMPOSITOR, "W.A. Mozart");
+        values.put(EstructuraBD.EstructuraOperas.COLUMN_NAME_YEAR, 1787);
+        // Insertamos los datos
+        db.insert(EstructuraBD.EstructuraOperas.TABLE_NAME_OPERAS, null, values);
+
+
+        // Borramos todas las filas excepto la primera
+      //  db.delete(EstructuraBD.EstructuraOperas.TABLE_NAME_OPERAS, where "_ID>1")
     }
 }
--- app/src/main/java/com/example/appbasedatos/SQLiteHelper.java
+++ app/src/main/java/com/example/appbasedatos/SQLiteHelper.java
@@ -6,24 +6,28 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.Nullable;
 
-/**Sirve de ayuda para crear la BD**/
-public class SQliteHelper extends SQLiteOpenHelper {
+/**
+ * Sirve de ayuda para crear la BD
+ **/
+public class SQLiteHelper extends SQLiteOpenHelper {
 
     public static final int DATABASE_VERSION = 1; // constantes para usar en el constructor
     public static final String DATABASE_NAME = "dbMusica.db";
 
-    public SQliteHelper(@Nullable Context context) {
+    public SQLiteHelper(@Nullable Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        //creamos la tabla si no existe
+        //Creamos la tabla si no existe
         db.execSQL(EstructuraBD.SQL_CREATE_ENTRIES);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+        // Borra la tabla y la crea de nuevo. No sería lo más correcto.
+        db.execSQL(EstructuraBD.SQL_DELETE_ENTRIES);
+        db.execSQL(EstructuraBD.SQL_CREATE_ENTRIES);
     }
 }
