12af09b8e5c63d460c339447d7f1ee2d8fc42de7
luisgava <luisgargallovaamonde@gmail.com>
2021-01-19 10:08:55 +0100

    actualizacion

--- app/src/main/java/com/example/appbasedatos/MainActivity.java
+++ app/src/main/java/com/example/appbasedatos/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.appbasedatos;
 
 import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
@@ -46,6 +47,52 @@ public class MainActivity extends AppCompatActivity {
         db.delete(EstructuraBD.EstructuraOperas.TABLE_NAME_OPERAS, "_ID>1", null);
 
         //Insertamos más datos con el método inserta()
+        inserta("Le Nozze di Figaro", "W. A. Mozart", 1786);
+        inserta("Giulio Cesare", "G. F. Haendel", 1724);
+        inserta("Orlando Furioso", "A. Vivaldi", 1727);
+        inserta("Montezuma", "C. H. Graun", 1755);
+        inserta("Starira", "F. Cavalli", 1656);
+        inserta("Griselda", "A. Sca rlatti", 1721);
+        inserta("Piramo e Tisbe", "J. A. Hasse", 1768);
+        inserta("Atenaide", " A. Vivaldi", 1728);
+        inserta("Tolomeo", "G. F. Haendel", 1728);
+        inserta("Armida", "J. Haydn", 1784);
+        inserta("Armide", "C. W. Gluck", 1777);
+        inserta("II Tutore Buriato", "V. Martin y Soler", 1775);
+        inserta("Berenice", "G. F. Haendel", 1737);
 
+        // Consulta para la base de datos.
+        // La query es como hacer un SELST * FROM OPERAS;
+        Cursor cursor = db.query("operas", null, null, null,
+                null, null, null);
+
+        db.close();
+    }
+
+    //creamos un método para insertar filas
+    private void inserta(String titulo, String compositor, int year) {
+        ContentValues values = new ContentValues();
+        values.put(EstructuraBD.EstructuraOperas.COLUMN_NAME_TITULO, titulo);
+        values.put(EstructuraBD.EstructuraOperas.COLUMN_NAME_COMPOSITOR, compositor);
+        values.put(EstructuraBD.EstructuraOperas.COLUMN_NAME_YEAR, year);
+        db.insert(EstructuraBD.EstructuraOperas.TABLE_NAME_OPERAS, null, values);
+    }
+
+    // Metodo para mostrar la tabla
+    private void mostrarTabla(Cursor c) {
+        //los mostramos en el cuadro de texto que tenemos en el layout
+        txtTexto.append("\n Tabla operas \n-----------");
+        c.moveToFirst();
+        int nfilas = c.getCount();
+        int ncolumnas = c.getColumnCount();
+        String fila = "\n";
+        for (int i = 0; i < nfilas; i++) {
+            fila = "\n";
+            for (int j = 0; j < ncolumnas; j++) {
+                fila = fila + c.getString(j) + " ";
+            }
+            txtTexto.append(fila);
+            c.moveToNext();
+        }
     }
 }
